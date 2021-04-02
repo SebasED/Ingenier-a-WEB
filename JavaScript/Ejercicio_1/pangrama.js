d472130084461825cc1072030dc86f24a889b2b0
SebasED <johns.estrada@udea.edu.co>
2021-04-01 21:48:10 -0500

    Solución del ejercicio 1 de JavaScript

---
+++ JavaScript/Ejercicio_1/pangrama.js
@@ -0,0 +1,25 @@
+
+function pangrama() {
+    var texto = document.getElementById("texto").value;
+    texto = texto.toLowerCase();
+    const alfabeto = "abcdefghijklmnñopqrstuvwxyz";
+    let alfabeto_arreglo = Array.from(alfabeto);
+    esta = false;
+    for (let index = 0; index < alfabeto.length; index++) {
+        let letra = alfabeto_arreglo[index];
+        
+        if (!texto.includes(letra)) {
+            document.getElementById("respuesta").innerHTML = "No es pangrama"
+            console.log("No es pangrama");
+            esta = false;
+            break;
+        }
+        esta = true;
+    }
+    
+    if (esta) {
+        document.getElementById("respuesta").innerHTML = "Es pangrama"
+        console.log("Es pangrama");
+    }
+    
+}
